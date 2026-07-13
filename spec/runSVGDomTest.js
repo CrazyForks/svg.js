93458599a8abe2c968fdca51e4a2971156708b3c
Ulrich-Matthias Schäfer <ulima.ums@googlemail.com>
2026-07-13 21:01:22 +0200

    Stop masking test failures

--- spec/runSVGDomTest.js
+++ spec/runSVGDomTest.js
@@ -1,10 +1,3 @@
-/*
-  This file has to be run with esm because node does not understand imports yet:
-  node -r esm ./spec/runSvgdomTest.js || true
-
-  Without "|| true" node reports a super long error when a test fails
- */
-
 import Jasmine from 'jasmine'
 const jasmine = new Jasmine()
 
